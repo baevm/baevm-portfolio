f94218a2499b12148978a3fcfb2f44078d11177c
dezzerlol <misha.6325@gmail.com>
2022-07-17 21:43:00 +0500

    locomotive added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from 'next'
+import { useRef } from 'react'
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 import { useSelector } from 'react-redux'
 import FloatingButton from '../components/FloatingButton/FloatingButton'
 import Buttons from '../components/MainPage/Buttons'
@@ -8,17 +10,26 @@ import Sidebar from '../components/Sidebar/Sidebar'
 import styles from '../styles/Index.module.scss'
 
 const Home: NextPage = () => {
+  const containerRef = useRef(null)
   return (
     <>
       <SEO title='Mikhail Baev' siteName='Frontend Developer' />
+
       <div className={`${styles.wrapper}`}>
         <Buttons />
-        <div className={styles.sidebar_container}>
-          <Sidebar />
-        </div>
-        <main className={styles.content_container}>
-          <Content />
-        </main>
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            smartphone: { smooth: true },
+            tablet: { smooth: true },
+            lerp: 0.03,
+            multiplier: 1.4,
+          }}
+          containerRef={containerRef}>
+          <main className={styles.content_container} ref={containerRef} data-scroll-section>
+            <Content />
+          </main>
+        </LocomotiveScrollProvider>
         <FloatingButton />
       </div>
     </>
