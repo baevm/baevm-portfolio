afde2576680f9ee64524eda36ac491843019da50
dezzerlol <misha.6325@gmail.com>
2022-07-30 18:56:53 +0500

    gif fix for nextjs prod build

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,7 +5,7 @@ import styles from './style.module.scss'
 const Loader = () => {
   return (
     <div className={styles.loader__container}>
-      <Image src='/804.png' width={64} height={64} quality={75} />
+      <img src='/804.png' />
     </div>
   )
 }
